dc5a558f8f71ac2e580cbc81fbe8c71a7ef4cc87
Priyanshugitnegi <negips11april@gmail.com>
2025-12-23 23:02:11 +0530

    Added render.yaml for deployment

--- biologist_project/settings.py
+++ biologist_project/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-change-this-in-production-please!!'
 DEBUG = True  # ← Set to False in production!
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1',]
+ALLOWED_HOSTS = ['*']
 
 # ──────────────────────────────────────
 # INSTALLED APPS
