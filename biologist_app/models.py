b22a645734f4dd90cf6ed34f675dd626e4ab1b4d
Priyanshugitnegi <negips11april@gmail.com>
2025-12-30 18:32:46 +0530

    Allow general and product enquiries

--- biologist_app/models.py
+++ biologist_app/models.py
@@ -244,18 +244,22 @@ class Enquiry(models.Model):
     product = models.ForeignKey(
         Product,
         related_name="enquiries",
-        on_delete=models.CASCADE
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
     )
 
     variant = models.ForeignKey(
         ProductVariant,
         related_name="enquiries",
-        on_delete=models.CASCADE
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
     )
 
     name = models.CharField(max_length=100)
     email = models.EmailField()
-    phone = models.CharField(max_length=20, blank=True)
+    phone = models.CharField(max_length=50, blank=True)
     message = models.TextField(blank=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
@@ -264,8 +268,9 @@ class Enquiry(models.Model):
         ordering = ["-created_at"]
 
     def __str__(self):
-        return f"{self.name} → {self.variant.catalog_number}"
-
+        if self.product:
+            return f"{self.name} → {self.product.name}"
+        return f"{self.name} → General Enquiry"
 
 # ==============================
 # TEAM MEMBER
